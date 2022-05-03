ba1be97e4b4aab88d6965542050664bb61710495
Stéphane Travostino <steph@combo.cc>
2022-05-03 12:15:23 +0100

    Moved more utility code into Installer

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-Pipeline implies extracting first if the file is an archive
-Also, the last implied step of the pipeline is running a `find . -name *.otf`" (depending on the font type) and those will be the font files.
-
 # TODO
 
 - [x] Search a font by name
-- [ ] github-release source implementation
-- [ ] Install a font from YAML recipe
-- [ ] Install a font by name
+- [X] github-release source implementation
+- [X] Install a font from YAML recipe
+- [X] Install a font by name
+- [ ] Abort if font is already installed
+- [ ] Handle font postprocess script
+- [ ] Handle .tar.gz archives
 - [ ] Host the repository on a github project
--- src/installer.cr
+++ src/installer.cr
@@ -1,8 +1,82 @@
 require "file_utils"
+require "http/client"
+require "mime"
+require "process"
 require "./recipe"
-require "./utils"
+
+MIME.init()
 
 class Installer
+  enum ArchiveType
+    #Tarball
+    Zip
+  end
+
+  def filename_from_content_disposition(cd)
+    if cd =~ /^attachment; filename=(.*)$/i
+      $~[1]
+    end
+  end
+
+  def archive_type(filename)
+    # if filename =~ /\.tar\.\w+$/
+    #   ArchiveType::Tarball
+    # elsif MIME.from_filename(filename) == "application/zip"
+    #   ArchiveType::Zip
+    # else
+    #   raise "#{filename}: unknown archive type"
+    # end
+
+    if MIME.from_filename(filename) == "application/zip"
+      ArchiveType::Zip
+    else
+      raise "#{filename}: unknown archive type"
+    end
+
+  end
+
+  def download_file(url, dir = Dir.tempdir)
+    HTTP::Client.get(url) do |resp|
+      if resp.status_code == 302 && (location = resp.headers["Location"]?)
+        return download_file(location, dir)
+      end
+
+      if resp.success?
+        filename = if (cd = resp.headers["Content-Disposition"]?)
+                     filename_from_content_disposition(cd)
+                   elsif (ct = resp.headers["Content-Type"]?)
+                     suffix = MIME.extensions(ct)
+                              .first?
+                              .try { |ext| ".#{ext}" }
+                     File.tempname(nil, suffix, dir: "")
+                   end
+
+        filename = File.tempname(dir: "") unless filename
+
+        path = Path.new(dir, filename)
+        File.open(path, "wb") do |f|
+          IO.copy(resp.body_io, f)
+        end
+
+        path.to_s
+      end
+    end
+  end
+
+  def unpack(file, dir)
+    case archive_type(file)
+    in ArchiveType::Zip
+      unpack_zip(file, dir)
+    end
+  end
+
+  def unpack_zip(file, dir)
+    p = Process.run("unzip", [ "-d", dir, file ])
+    unless p.success?
+      raise "Unpacking #{file} failed with status code ${p.exit_status}"
+    end
+  end
+
   def format_glob(format : String, dir)
     Path.new(dir, "**", "*.#{format}")
   end
@@ -23,12 +97,12 @@ class Installer
       # 1. Download file
       download_url = recipe.source.download_url().not_nil!
       puts "Downloading #{download_url} ..."
-      download_path = Utils.download_file(download_url, temp_dir).not_nil!
+      download_path = download_file(download_url, temp_dir).not_nil!
       #puts "Saved as #{download_path}"
 
       # 2. Unpack
       puts "Unpacking #{download_path}..."
-      Utils.unpack(download_path, temp_dir)
+      unpack(download_path, temp_dir)
 
       # 3. Run script
       if recipe.script
