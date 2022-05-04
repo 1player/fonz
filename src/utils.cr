24634f1368ec5dcd28fbdf01db60039381014ee1
Stéphane Travostino <steph@combo.cc>
2022-05-04 14:32:47 +0100

    make Utils module more idiomatic

--- src/utils.cr
+++ src/utils.cr
@@ -1,17 +1,15 @@
 module Utils
-  extend self
-
-  def user_fonts_directory
+  def self.user_fonts_directory
     Path.new(xdg_data_home(), "fonts")
   end
 
   # https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
 
-  def xdg_config_home : String
+  def self.xdg_config_home : String
     ENV["XDG_CONFIG_HOME"]? || Path["~/.config"].expand(home: true).to_s
   end
 
-  def xdg_data_home : String
+  def self.xdg_data_home : String
     ENV["XDG_DATA_HOME"]? || Path["~/.local/share"].expand(home: true).to_s
   end
 end
